20dfac2f1dd643b50f895ff33dc92ff26550a20f
Hovering-Ace <hk3326197@gmail.com>
2025-08-13 13:14:33 +0530

    modified mailSender

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,26 +1,28 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const mailSender = async (email,title,body) => {
+const mailSender = async (email, title, body) => {
     try {
         let transporter = nodemailer.createTransport({
-            host:process.env.Mail_HOST,
-            auth:{
+            host: process.env.MAIL_HOST,
+            port: 465,
+            secure: true,
+            auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
-            }
-        })
+            },
+        });
         let info = await transporter.sendMail({
-           from: `"StudyNotion - JATIN" <${process.env.MAIL_USER}>`,
-            to:`${email}`,
-            subject:`${title}`,
-            html:`${body}`
+            from: `"StudyNotion - JATIN" <${process.env.MAIL_USER}>`,
+            to: `${email}`,
+            subject: `${title}`,
+            html: `${body}`
         })
         //console.log(info);
         return info;
     } catch (error) {
         console.log(error.message);
-        
+
     }
 }
 
